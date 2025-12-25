e7f3ca2411a7ad2a8e1fcc7d7d505fb350c2f6e1
janhmn <jan.hmn@googlemail.com>
2025-12-25 17:43:45 +0100

    fix: removal of unnecessary code

--- main.py
+++ main.py
@@ -6,14 +6,10 @@ with Picamera2() as camera:
     # Optional: Set resolution
     camera.resolution = (1024, 768)
 
-    # Start a preview (optional, helpful for positioning)
-    camera.start_preview()
-
     # Camera warm-up time (essential for exposure/white balance)
     time.sleep(2)
 
     for i in range(10):
-        time.sleep(10)
+        print(i)
         camera.capture_file("src/image{0:04d}.jpg".format(i))
-    # Capture the image
-    camera.stop_preview()
+        time.sleep(10)
